745c12272aa15ca217cfad06ad046d24878679de
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2019-09-11 14:17:45 +0200

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -3,10 +3,52 @@ public class TestClass
 {
     public static void main(String[] args)
     {
+        int option = 0;
+
+        boolean run = true;
+
         Scanner input = new Scanner(System.in);
 
-        GroceryList groceryList = new GroceryList();
+        //GroceryList groceryList = new GroceryList();
+        GroceryList2 groceryList = new GroceryList2();
+
+        while (run)
+        {
+            option = input.nextInt();
+            switch (option)
+            {
+                case 0:
+                System.out.println("Input a new item name: ");
+                input = new Scanner(System.in);
+                String newItemName = input.nextLine();
 
 
+                System.out.println("Input a new item quantity: ");
+                int newItemQuantity = input.nextInt();
+
+                System.out.println("Input a new item pricePrUnit: ");
+                double newItemPricePrUnit = input.nextDouble();
+
+                GroceryItemOrder groceryItemOrder = new GroceryItemOrder(newItemName, newItemQuantity, newItemPricePrUnit);
+
+                try
+                {
+                    groceryList.addItem(groceryItemOrder);
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e.getMessage());
+                }
+                    break;
+
+                case 1:
+                    groceryList.toString();
+                    break;
+
+                case 2:
+                    run = false;
+                    break;
+            }
+        }
     }
 }
